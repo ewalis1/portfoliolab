83288beabcf6e6d3a3de7d546b8469e762edf2b4
ewalis1 <73714272+ewalis1@users.noreply.github.com>
2021-06-28 13:28:19 +0200

    added four steps home section

--- src/components/HomeFourSteps.js
+++ src/components/HomeFourSteps.js
@@ -1,5 +1,48 @@
+import {Link} from 'react-router-dom';
+
 const HomeFourSteps = () => {
-  return <></>;
+  return (
+    <div className="fourSteps">
+      <div className="fourSteps__header">
+        <h1>Wystarczą 4 proste kroki</h1>
+        <div className="decorate"></div>
+      </div>
+      <div className="fourSteps__main">
+        <div className="fourSteps__container">
+          <div className="step">
+            <div className="icon icon1"></div>
+            <p>Wybierz rzeczy</p>
+            <p>ubrania, zabawki, sprzęt i inne</p>
+          </div>
+          <div className="step">
+            <div className="icon icon2"></div>
+            <p>Spakuj je</p>
+            <p>skorzystaj z worków na śmieci</p>
+          </div>
+          <div className="step">
+            <div className="icon icon3"></div>
+            <p>
+              Zdecyduj komu <br />
+              chcesz oddać
+            </p>
+            <p>wybierz zaufane miejsce</p>
+          </div>
+          <div className="step">
+            <div className="icon icon4"></div>
+            <p>Zamów kuriera</p>
+            <p>
+              kurier przyjedzie <br />w dogodnym terminie
+            </p>
+          </div>
+        </div>
+      </div>
+      <div className="fourSteps__footer">
+        <Link to="/logowanie" className="btn btn-big">
+          oddaj rzeczy
+        </Link>
+      </div>
+    </div>
+  );
 };
 
 export default HomeFourSteps;
