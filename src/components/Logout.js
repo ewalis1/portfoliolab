90d33e88db6bb510d6675dde1c75df22e0beae6d
ewalis1 <73714272+ewalis1@users.noreply.github.com>
2021-07-02 22:21:55 +0200

    added logout section

--- src/components/Logout.js
+++ src/components/Logout.js
@@ -1,5 +1,19 @@
+import {Link} from 'react-router-dom';
+
 const Logout = () => {
-  return <></>;
+  return (
+    <div className="logout">
+      <h1>Wylogowanie nastąpiło <br/>pomyślnie!</h1>
+      <div className="decorate"></div>
+      <Link
+        to="/
+      "
+        className="btn btn-log"
+      >
+        Strona główna
+      </Link>
+    </div>
+  );
 };
 
 export default Logout;
