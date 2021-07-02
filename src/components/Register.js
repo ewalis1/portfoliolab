bc3220459387c146f24a2d18dc30b6e70ec5f666
ewalis1 <73714272+ewalis1@users.noreply.github.com>
2021-07-02 22:02:48 +0200

    added register section styling and routing

--- src/components/Register.js
+++ src/components/Register.js
@@ -1,5 +1,42 @@
+import {Link} from 'react-router-dom';
+
 const Register = () => {
-  return <></>;
+  const handleSubmit = (e) => {
+    e.preventDefault();
+  };
+  return (
+    <div className="register">
+      <h1>Załóż konto</h1>
+      <div className="decorate"></div>
+      <div className="register__container">
+        <form onSubmit={handleSubmit} className="form-register">
+          <div className="form-register__container">
+            <label for="register">Email</label>
+            <br />
+            <input type="email" id="register"></input>
+            <label for="password" className="password">
+              Hasło
+            </label>
+            <br />
+            <input type="password" id="password"></input>
+            <label for="password-again" className="password">
+              Powtórz hasło
+            </label>
+            <br />
+            <input type="password-again" id="password"></input>
+          </div>
+        </form>
+      </div>
+      <div className="register__links">
+        <Link to="/logowanie" className="btn-log">
+          Zaloguj się
+        </Link>
+        <Link to="/oddaj-rzeczy" className="btn btn-log">
+          Załóż konto
+        </Link>
+      </div>
+    </div>
+  );
 };
 
 export default Register;
