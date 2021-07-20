50cc7364ccc04158798f5722dfab043afd5b74dd
ewalis1 <73714272+ewalis1@users.noreply.github.com>
2021-07-20 10:29:47 +0200

    changed rendering on bigger screens and oddarz-rzeczy buttons in header, depending on being logged or not

--- src/components/HomeHeader.js
+++ src/components/HomeHeader.js
@@ -1,6 +1,9 @@
 import {Link} from 'react-router-dom';
 
 const HomeHeader = () => {
+  const isLoggedIn = window.localStorage.getItem('myValueInLocalStorage')
+    ? true
+    : false;
   return (
     <div className="home-header">
       <div className="jumbo">
@@ -13,12 +16,25 @@ const HomeHeader = () => {
           </h1>
           <div className="decorate"></div>
           <div className="jumbo__buttons">
-            <Link to="/logowanie" className="btn btn-big">
-              oddaj rzeczy
-            </Link>
-            <Link to="/logowanie" className="btn btn-big">
-              zorganizuj zbiórkę
-            </Link>
+            {isLoggedIn ? (
+              <>
+                <Link to="/oddaj-rzeczy" className="btn btn-big">
+                  oddaj rzeczy
+                </Link>
+                <Link to="/oddaj-rzeczy" className="btn btn-big">
+                  zorganizuj zbiórkę
+                </Link>
+              </>
+            ) : (
+              <>
+                <Link to="/logowanie" className="btn btn-big">
+                  oddaj rzeczy
+                </Link>
+                <Link to="/logowanie" className="btn btn-big">
+                  zorganizuj zbiórkę
+                </Link>
+              </>
+            )}
           </div>
         </div>
       </div>
--- src/components/LoggedIn.js
+++ src/components/LoggedIn.js
@@ -1,5 +1,15 @@
+const {default: HomeContact} = require('./HomeContact');
+const {default: LoggedInForm} = require('./LoggedInForm');
+const {default: LoggedInHeader} = require('./LoggedInHeader');
+
 const LoggedIn = () => {
-  return <></>;
+  return (
+    <>
+      <LoggedInHeader />
+      <LoggedInForm />
+      <HomeContact />
+    </>
+  );
 };
 
 export default LoggedIn;
